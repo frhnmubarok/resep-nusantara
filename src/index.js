ef6a2f54d91de87808296e51c372f7740bbacebb
frhnmubarok <farhanmubarok104@gmail.com>
2020-12-17 16:38:58 +0700

    adding random recipe feature

--- src/index.js
+++ src/index.js
@@ -7,23 +7,16 @@ const BASE_URL = `https://masak-apa.tomorisakura.vercel.app`;
 module.exports = async function App(context) {
 	if (context.event.isText) {
 		if (context.event.text.split(" ").includes("/cari")) {
-			await context.sendText(`OK`);
+			let splitted = context.event.text.split(" ");
+			splitted.shift();
+			let query = splitted.join(" ");
+			return withProps(searchRecipe, { resep: query });
 		}
 	}
-	// if (context.event.isPayload) {
-	// 	//context.payload("apakah kamu eeq");
-	// 	if (context.event.payload === "sd") {
-	// 		context.payload("asd");
-	// 	}
-	// 	await context.sendText(`received the payload: ${context.event.payload}`);
-	// }
+
 	return router([
-		//text("resep", recipeDetails),\
 		line.follow(handleFollow),
-		text(
-			"resep-nasi-goreng-jawa",
-			withProps(recipeDetails, { resep: "resep-nasi-goreng-jawa" })
-		),
+		text(["/random", "/Random"], randomRecipe),
 		text(["/kategori", "Kategori"], recipeCategory),
 		text("/help", handleHelp),
 		line.postback(handlePostback),
@@ -35,8 +28,13 @@ async function handleFollow(context) {
 	await context.sendText(
 		`Hai!. Terima kasih telah follow akun ini ${String.fromCodePoint(
 			0x100078
-		)}. Kami menyediakan resep-resep masakan dari Indonesia. Kalian bisa cari resep berdasarkan kategori, atau berdasarkan kata kunci yang kalian inginkan (contoh: nasi goreng). Selamat memasak ${String.fromCodePoint(
+		)}. Kami menyediakan resep-resep masakan dari Indonesia. Kalian bisa cari resep berdasarkan kategori, atau berdasarkan kata kunci yang kalian inginkan (contoh: nasi goreng). Kalau kalian memilih menu Surprise Me!, kami akan mengirimkan resep masakan secara acak. Selamat memasak ${String.fromCodePoint(
 			0x10008d
+		)}`
+	);
+	await context.sendText(
+		`*catatan : apabila bot tidak merespon ketika mengirimkan perintah, silahkan kirimkan perintah lagi setelah beberapa detik. Mohon maaf atas ketidaknyamanannya ${String.fromCodePoint(
+			0x100092
 		)}`
 	);
 	await context.sendText(
@@ -66,44 +64,50 @@ async function handlePostback(context) {
 
 async function handleHelp(context) {
 	try {
-		await context.sendText(
-			"Berikut perintah bot : \n/kategori = Mencari resep berdasarkan kategori yang tersedia \n/artikel = Menampilkan artikel seputar masakan \n/cari = Mencari resep berdasarkan kata kunci, contoh : /cari nasi goreng"
-		);
-		const quickReply = {
-			items: [
-				{
-					type: "action",
-					action: {
-						type: "message",
-						label: "Kategori",
-						text: "/kategori",
+		await context.sendFlex("Menu help", {
+			type: "bubble",
+			size: "kilo",
+			body: {
+				type: "box",
+				layout: "vertical",
+				contents: [
+					{
+						type: "button",
+						action: {
+							type: "message",
+							label: "Kategori",
+							text: "/kategori",
+						},
+						style: "primary",
+						margin: "sm",
+						height: "sm",
 					},
-				},
-				{
-					type: "action",
-					action: {
-						type: "message",
-						label: "Artikel",
-						text: "/artikel",
+					{
+						type: "button",
+						action: {
+							type: "message",
+							label: "Surprise Me!",
+							text: "/random",
+						},
+						style: "primary",
+						margin: "sm",
+						height: "sm",
 					},
-				},
-				{
-					type: "action",
-					action: {
-						type: "postback",
-						label: "Cari",
-						data: "pb-cari",
+					{
+						type: "button",
+						action: {
+							type: "postback",
+							label: "Cari",
+							data: "pb-cari",
+						},
+						style: "primary",
+						margin: "sm",
+						height: "sm",
 					},
-				},
-			],
-		};
-		await context.send([
-			{
-				type: "text",
-				text: "Atau bisa langsung menekan tombol dibawah ini",
-				quickReply,
+				],
+				backgroundColor: "#81ecec",
 			},
-		]);
+		});
 	} catch (err) {
 		console.log(err);
 	}
@@ -114,29 +118,40 @@ const recipeCategory = async (context) => {
 		const quickReply = {
 			items: [],
 		};
+		const contents = [];
 		const category = await fetch(`${BASE_URL}/api/categorys/recipes/`);
 		let response = await category.json();
 		let data = response.results;
-		data.slice(1, 6).forEach((categories) => {
+		data.forEach((categories) => {
 			let categoryObj = {
-				type: "action",
-				action: {
-					type: "postback",
-					label: categories.category,
-					data: `${categories.key} kategori`,
-					//text: categories.key,
+				type: "bubble",
+				size: "kilo",
+				body: {
+					type: "box",
+					layout: "vertical",
+					contents: [
+						{
+							type: "button",
+							action: {
+								type: "postback",
+								label: categories.category,
+								data: `${categories.key} kategori`,
+							},
+							style: "primary",
+							height: "md",
+							margin: "none",
+						},
+					],
+					backgroundColor: "#81ecec",
 				},
 			};
-			quickReply.items.push(categoryObj);
+			contents.push(categoryObj);
 		});
 
-		await context.send([
-			{
-				type: "text",
-				text: "Berikut kategori masakan",
-				quickReply,
-			},
-		]);
+		await context.sendFlex("Category menu", {
+			type: "carousel",
+			contents,
+		});
 	} catch (err) {
 		console.log(err);
 	}
@@ -337,16 +352,7 @@ const recipeDetails = async (context, props) => {
 				contents: [],
 			},
 		];
-		data.needItem.forEach((item) => {
-			let itemObj = {
-				type: "text",
-				text: `- ${item.item_name}`,
-				size: "sm",
-				wrap: true,
-				offsetTop: "xs",
-			};
-			bahan.push(itemObj);
-		});
+
 		data.ingredient.forEach((item) => {
 			let ingredientObj = {
 				type: "text",
@@ -357,6 +363,7 @@ const recipeDetails = async (context, props) => {
 			};
 			bahan.push(ingredientObj);
 		});
+
 		data.step.forEach((item) => {
 			let stepObj = {
 				type: "text",
@@ -367,10 +374,401 @@ const recipeDetails = async (context, props) => {
 			};
 			steps.push(stepObj);
 		});
+
 		if (data.thumb === null) {
 			data.thumb =
 				"https://www.hopkinsmedicine.org/-/media/feature/noimageavailable.ashx";
 		}
+
+		await context.sendFlex("This is an advanced flex", {
+			type: "bubble",
+			size: "giga",
+			hero: {
+				type: "image",
+				url: data.thumb,
+				size: "full",
+				aspectRatio: "20:13",
+				aspectMode: "cover",
+			},
+			body: {
+				type: "box",
+				layout: "vertical",
+				contents: [
+					{
+						type: "text",
+						text: data.title,
+						weight: "bold",
+						size: "lg",
+						align: "center",
+						wrap: true,
+					},
+					{
+						type: "box",
+						layout: "vertical",
+						margin: "lg",
+						spacing: "sm",
+						contents: [
+							{
+								type: "box",
+								layout: "baseline",
+								spacing: "sm",
+								contents: [
+									{
+										type: "text",
+										text: "Waktu",
+										color: "#aaaaaa",
+										size: "sm",
+										flex: 3,
+									},
+									{
+										type: "text",
+										text: data.times,
+										wrap: true,
+										color: "#666666",
+										size: "sm",
+										flex: 10,
+									},
+								],
+							},
+							{
+								type: "box",
+								layout: "baseline",
+								spacing: "sm",
+								contents: [
+									{
+										type: "text",
+										text: "Porsi",
+										flex: 3,
+										size: "sm",
+										color: "#aaaaaa",
+									},
+									{
+										type: "text",
+										text: data.servings,
+										flex: 10,
+										size: "sm",
+										color: "#666666",
+									},
+								],
+							},
+							{
+								type: "box",
+								layout: "baseline",
+								contents: [
+									{
+										type: "text",
+										text: "Kesulitan",
+										flex: 3,
+										size: "sm",
+										color: "#aaaaaa",
+									},
+									{
+										type: "text",
+										text: data.dificulty,
+										flex: 10,
+										size: "sm",
+										color: "#666666",
+										wrap: true,
+									},
+								],
+								spacing: "sm",
+							},
+							{
+								type: "box",
+								layout: "baseline",
+								contents: [
+									{
+										type: "text",
+										text: "Author",
+										flex: 3,
+										size: "sm",
+										color: "#aaaaaa",
+									},
+									{
+										type: "text",
+										text: data.author.user,
+										flex: 10,
+										size: "sm",
+										color: "#666666",
+									},
+								],
+								spacing: "sm",
+							},
+						],
+					},
+					{
+						type: "separator",
+						margin: "sm",
+					},
+					{
+						type: "box",
+						layout: "vertical",
+						contents: [
+							{
+								type: "box",
+								layout: "vertical",
+								contents: bahan,
+							},
+						],
+					},
+					{
+						type: "separator",
+						margin: "sm",
+					},
+					{
+						type: "box",
+						layout: "vertical",
+						contents: [
+							{
+								type: "box",
+								layout: "vertical",
+								contents: steps,
+							},
+						],
+					},
+				],
+				spacing: "none",
+				margin: "none",
+				borderWidth: "none",
+				cornerRadius: "none",
+				paddingAll: "lg",
+			},
+			styles: {
+				footer: {
+					separator: true,
+				},
+			},
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const searchRecipe = async (context, props) => {
+	try {
+		const contents = [];
+		const details = await fetch(`${BASE_URL}/api/search/?q=${props.resep}`);
+		let response = await details.json();
+		let data = response.results;
+		data.splice(0, 8).forEach((recipe) => {
+			let recipeObj = {
+				type: "bubble",
+				size: "kilo",
+				hero: {
+					type: "image",
+					url: recipe.thumb,
+					size: "full",
+					aspectRatio: "20:13",
+					aspectMode: "cover",
+				},
+				body: {
+					type: "box",
+					layout: "vertical",
+					contents: [
+						{
+							type: "text",
+							text: recipe.title,
+							weight: "bold",
+							size: "md",
+							wrap: true,
+						},
+						{
+							type: "box",
+							layout: "vertical",
+							margin: "lg",
+							spacing: "sm",
+							contents: [
+								{
+									type: "box",
+									layout: "baseline",
+									spacing: "sm",
+									contents: [
+										{
+											type: "icon",
+											url:
+												"https://www.flaticon.com/svg/static/icons/svg/850/850960.svg",
+											size: "sm",
+										},
+										{
+											type: "text",
+											text: "Waktu",
+											color: "#aaaaaa",
+											size: "sm",
+											flex: 6,
+										},
+										{
+											type: "text",
+											text: recipe.times,
+											wrap: true,
+											color: "#666666",
+											size: "sm",
+											flex: 10,
+										},
+									],
+								},
+								{
+									type: "box",
+									layout: "baseline",
+									spacing: "sm",
+									contents: [
+										{
+											type: "icon",
+											url:
+												"https://www.flaticon.com/svg/static/icons/svg/608/608857.svg",
+											size: "sm",
+										},
+										{
+											type: "text",
+											text: "Porsi",
+											color: "#aaaaaa",
+											size: "sm",
+											flex: 6,
+										},
+										{
+											type: "text",
+											text: recipe.serving,
+											wrap: true,
+											color: "#666666",
+											size: "sm",
+											flex: 10,
+										},
+									],
+								},
+								{
+									type: "box",
+									layout: "baseline",
+									contents: [
+										{
+											type: "icon",
+											url:
+												"https://www.flaticon.com/svg/static/icons/svg/571/571685.svg",
+											size: "sm",
+										},
+										{
+											type: "text",
+											text: "Kesulitan",
+											size: "sm",
+											flex: 6,
+											color: "#aaaaaa",
+											offsetStart: "sm",
+										},
+										{
+											type: "text",
+											text: recipe.difficulty,
+											wrap: true,
+											color: "#666666",
+											size: "sm",
+											flex: 10,
+											offsetStart: "sm",
+										},
+									],
+								},
+							],
+						},
+					],
+				},
+				footer: {
+					type: "box",
+					layout: "vertical",
+					spacing: "sm",
+					contents: [
+						{
+							type: "button",
+							style: "primary",
+							height: "sm",
+							action: {
+								type: "postback",
+								label: "Lihat Resep",
+								data: `${recipe.key} resep`,
+							},
+						},
+					],
+					flex: 0,
+					backgroundColor: "#81ecec",
+				},
+			};
+			contents.push(recipeObj);
+		});
+		await context.sendFlex("This is a carousel flex", {
+			type: "carousel",
+			contents,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const randomRecipe = async (context) => {
+	try {
+		const rand = Math.floor(Math.random() * 10);
+		const randFetch = await fetch(`${BASE_URL}/api/recipes/${rand}`);
+		const randResponse = await randFetch.json();
+		const randData = randResponse.results[rand].key;
+		const details = await fetch(`${BASE_URL}/api/recipe/${randData}`);
+		let response = await details.json();
+		let data = response.results;
+		let bahan = [
+			{
+				type: "text",
+				size: "md",
+				margin: "sm",
+				wrap: true,
+				offsetTop: "sm",
+				text: "Bahan-bahan",
+				weight: "bold",
+				decoration: "underline",
+				align: "start",
+				offsetBottom: "none",
+				position: "relative",
+				offsetEnd: "none",
+				contents: [],
+			},
+		];
+		let steps = [
+			{
+				type: "text",
+				size: "md",
+				margin: "sm",
+				wrap: true,
+				offsetTop: "sm",
+				text: "Cara membuat",
+				weight: "bold",
+				decoration: "underline",
+				align: "start",
+				offsetBottom: "none",
+				position: "relative",
+				offsetEnd: "none",
+				contents: [],
+			},
+		];
+
+		data.ingredient.forEach((item) => {
+			let ingredientObj = {
+				type: "text",
+				text: `- ${item}`,
+				size: "sm",
+				wrap: true,
+				margin: "xs",
+			};
+			bahan.push(ingredientObj);
+		});
+
+		data.step.forEach((item) => {
+			let stepObj = {
+				type: "text",
+				text: `${item}`,
+				size: "sm",
+				wrap: true,
+				margin: "md",
+			};
+			steps.push(stepObj);
+		});
+
+		if (data.thumb === null) {
+			data.thumb =
+				"https://www.hopkinsmedicine.org/-/media/feature/noimageavailable.ashx";
+		}
+
 		await context.sendFlex("This is an advanced flex", {
 			type: "bubble",
 			size: "giga",
@@ -539,21 +937,4 @@ async function unknown(context) {
 	await context.sendText(
 		"Mohon maaf, perintah tidak diketahui. Silakan ketik /help untuk melihat perintah bot"
 	);
-}
-
-// const weather = (context) => {
-// 	fetch(
-// 		"https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=metric&APPID=fd00acb497f50e8a216830219f3b24c4"
-// 	)
-// 		.then((response) => response.json())
-// 		.then((data) => context.replyText(data.main.feels_like))
-// 		.catch((err) => console.log(err));
-// };
-
-async function getWeather() {
-	const weather = await fetch(
-		"https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=metric&APPID=fd00acb497f50e8a216830219f3b24c4"
-	);
-	let response = await weather.json();
-	console.log(response.main.feels_like);
 }
